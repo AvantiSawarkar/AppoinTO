4be408ff86901e46dc1f75524cf1d01ecb3a31e6
AvantiSawarkar <avanti0405@gmail.com>
2026-04-17 18:28:40 +0530

    Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,12 +1,36 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Footer() {
+	const footerStyle={
+		backgroundColor:"#0096c7",
+	};
+	const textStyle={
+		fontFamily:"Times New Roman",
+		color:"#fff",
+		fontWeight:"30px"
+	};
+	const favIcon={
+		color:"#fff",
+		fontSize:"25px",
+		fontWeight: "bold",
+	};
+	const textBold={
+		fontWeight:"bold",
+		
+	};
 	return (
-		<footer className="bg-dark text-white p-4 mt-5">
-			<div className="container text-center">
-				<p>Contact: appoinTO@email.com | +91 1234567890</p>
-				<p>Follow us: Facebook | X | Instagram</p>
-				<p>© 2026 AppoinTO</p>
+		<footer className="p-4 mt-5" style={footerStyle}>
+			<div className="container text-center" style={textStyle}>
+				<p style={textBold}>Contact Us:</p>
+				<p> appoinTO@email.com | +91 1234567890</p>
+				<p style={textBold}>Follow us:</p>
+				<p> 
+					<Link to="/facebook" style={favIcon}><i className="fa-brands fa-facebook"></i></Link>
+					&ensp;&ensp;
+					<Link to="/twitter" style={favIcon}><i className="fa-brands fa-x-twitter"></i></Link> 
+					&ensp;&ensp;
+					<Link to="/instagram" style={favIcon}><i className="fa-brands fa-instagram"></i></Link></p>
+				<p style={textBold}>© 2026 AppoinTO</p>
 			</div>
 		</footer>
 	);
